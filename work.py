bb45810130fabd5ad7b2f2386687282f52ca79d2
Zakir Khan <zakir.khan@se-co.de>
2017-12-18 20:08:52 +0100

    changed a bit

--- work.py
+++ work.py
@@ -49,15 +49,15 @@ def getInOutDegreesnj(G,nj):
     inDegreeList = {}
     outDegreeList = {}
     combinedDegreeList = {}
-    for i in G:
+    for i in range(1,nj):
         inDegreeList[i] = 0
         outDegreeList[i] = 0
         combinedDegreeList[i] = 0
     for j in range(1,nj):
         cd = G[j][0]
         inDegreeList[cd] += 1
-        outDegreeList[i] += 1
-        combinedDegreeList[i] += 1
+        outDegreeList[j] += 1
+        combinedDegreeList[j] += 1
         combinedDegreeList[cd] +=1
     return inDegreeList,outDegreeList, combinedDegreeList
 
@@ -83,16 +83,11 @@ def probabilityNodes(G,start,end):
     for key in c:
         total_nodes = key +1
     p = [float(key)/(total_nodes+1) for x, key in c.iteritems()]
-    print p
     return np.random.choice(arre,1,p)[0] # Return the random node generated from the loop
     #return p
 def main():
-    randomGraphProb = randGraphWithProbability(4) # We get the random graph where t =1000 as mentioned in Task1
+    randomGraphProb = randGraphWithProbability(7) # We get the random graph where t =1000 as mentioned in Task1
     print randomGraphProb
-    inDegreeP,outDegreeP,CombinedDegreeP = getInOutDegrees(randomGraphProb)
-    print CombinedDegreeP
-    plt.bar(CombinedDegreeP.keys(), CombinedDegreeP.values(), color='g')
-    plt.show()
     #plt.bar(CombinedDegree.keys(), CombinedDegree.values(), color='g')
     #plt.show()
 
